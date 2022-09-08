e5625dec792d080797e701c1e3d8c1e40aebfe69
sbudimac <sbudimac@gmail.com>
2022-09-08 23:09:31 +0200

    Preliminary login logic, added model library project

---
+++ NoteCraftMAUI/NoteCraftModels/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteCraftModels
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
